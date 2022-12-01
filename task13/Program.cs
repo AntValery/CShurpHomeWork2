e3302f5992b49d4e6e12e26f91c716a836b6fbdc
1valerich1@mail.ru <1valerich1@mail.ru>
2022-12-01 09:36:52 +0400

    Added task13

---
+++ task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int tmp = n;
+
+int count = 0;
+while(tmp != 0){
+    tmp /= 10;
+    count++;
+}
+
+if(count < 3) Console.WriteLine("В числе меньше 3 цифр");
+else Console.WriteLine((int)Math.Abs(((n / Math.Pow(10, (count - 3))) % 10)));
