d5c5883a94aaef9a14af4a5b62704b2722f29598
1valerich1@mail.ru <1valerich1@mail.ru>
2022-12-01 09:46:12 +0400

    Added task15

---
+++ task15/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 7) Console.WriteLine("Число не является днём недели");
+else if (n > 5 && n <= 7) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
